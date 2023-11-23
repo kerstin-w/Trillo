4beae76061f1a3b182a9db7a6c54c26857a3ae80
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-11-23 18:48:17 +0100

    feat: Create the side nav and style it

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// Update the content of the element with the current year
+document.getElementById("currentYear").textContent = currentYear;
